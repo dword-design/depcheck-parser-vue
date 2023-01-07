d9c55d9b575b9d57b9c6fc567d9550dd29319cac
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-01-07 15:05:48 +0000

    fix: upgrades

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,11 +1,38 @@
 import { endent } from '@dword-design/functions'
 import depcheck from 'depcheck'
 import { outputFile } from 'fs-extra'
+import P from 'path'
 import withLocalTmpDir from 'with-local-tmp-dir'
 
 import self from '.'
 
 export default {
+  error: () =>
+    withLocalTmpDir(async () => {
+      await outputFile(
+        'pages/index.vue',
+        endent`
+          <template>
+            <foo>
+          </template>
+        `
+      )
+
+      const result = await depcheck('.', {
+        package: {
+          dependencies: {
+            foo: '^1.0.0',
+          },
+        },
+        parsers: {
+          '**/*.vue': self,
+        },
+      })
+      expect(result.dependencies).toEqual(['foo'])
+      expect(result.invalidFiles[P.resolve('pages', 'index.vue')]).toEqual(
+        new Error('tag <foo> has no matching end tag.')
+      )
+    }),
   'unused dependency': () =>
     withLocalTmpDir(async () => {
       const result = await depcheck('.', {
